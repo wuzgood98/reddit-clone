e129f2c68d47ae591621044aa28fbb7d7ed9bbc5
Gideon  Addo <gideonaddoofori@gmail.com>
2023-04-04 19:46:43 +0000

    Add meta tags in Head component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className='bg-redditLightBlue'>
+      <Head>
+        <meta name="description" content="A clone of the reddit web app." />
+        <meta name="author" content="Gideon Addo" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+        <meta
+          name="keywords"
+          content="NextJS, JavaScript, TypeScript, React, Tailwindcss, Firebase, Authentication, SSR, RSC"
+        />
+        <meta name="theme-color" content="#FFF" />
+      </Head>
+      <body className="bg-redditLightBlue">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
